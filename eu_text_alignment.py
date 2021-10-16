289d121f03138e585ea9aaa6f87eb6d79b5cfbe6
stephan <smuts1989@gmail.com>
2021-10-16 12:50:27 +0200

    Trying with Wasserstain distances

--- eu_text_alignment.py
+++ eu_text_alignment.py
@@ -1,10 +1,15 @@
 from collections import Counter
-
+import time
 import spacy
 import pandas as pd
 import text_similarity
 from bs4 import BeautifulSoup
 from sentence_transformers import SentenceTransformer
+
+# Try use multiple cores with pandas
+import pandas as pd
+import dask.dataframe as dd
+
 
 
 def get_html_text(html_document):
@@ -176,19 +181,105 @@ def sim_score_pos(spacy_english_sentence,
         danish_verbs.append(temp_danish_verbs)"""
 
 
+def calc_sim_score(x, danish_column_name="Danish Spacy"):
+    # Verbs
+    en_verbs = [verb for verb in x['English Spacy'] if verb.pos_ == "VERB"]
+    da_verbs = [verb for verb in x[danish_column_name] if verb.pos_ == "VERB"]
+    """verbs_best_match = []
+    for en_verb in en_verbs:
+        temp_match_sim = 0
+        temp_match_words = []
+        for da_verb in da_verbs:
+            new_match = en_verb.similarity(da_verb)
+            if new_match > temp_match_sim:
+                temp_match_sim = new_match
+                temp_match_words = [en_verb, da_verb]
+        verbs_best_match.append((temp_match_sim, temp_match_words))"""
+
+    # Nouns
+    en_nouns = [noun for noun in x['English Spacy'] if noun.pos_ == "NOUN"]
+    da_nouns = [noun for noun in x[danish_column_name] if noun.pos_ == "NOUN"]
+    """nouns_best_match = []
+    for en_noun in en_nouns:
+        temp_match_sim = 0
+        temp_match_words = []
+        for da_noun in da_nouns:
+            new_match = en_noun.similarity(da_noun)
+            if new_match > temp_match_sim:
+                temp_match_sim = new_match
+                temp_match_words = [en_noun, da_noun]
+        nouns_best_match.append((temp_match_sim, temp_match_words))"""
+
+    # Overall
+    #da = nlp_multi(x['Danish'])
+    #en = nlp_multi(x['English'])
+
+    #results = x['English Spacy'].similarity(x['Danish Spacy']), verbs_best_match, nouns_best_match
+    #results = [en_verbs, da_verbs, en_nouns, da_nouns]
+    if not isinstance(x[danish_column_name], str):
+        results = x['English Spacy'].similarity(x[danish_column_name])
+    else:
+        results = 0.0
+    return results
+
+
 if __name__ == "__main__":
-    """    
-    1. Match all sentences in the English list.
-    1.1. 
-    """
+    start = time.time()
+    # da_en statistics (pandas parallel - confirmed)
+    df = pd.read_csv("vikingeskibsmuseet_da_en_statistic.csv")
+    print("Length of total df: {}".format(len(df)))
+    df = pd.DataFrame(df[:100])
+    print(df.head())
+    exit()
+    nlp_multi = spacy.load("xx_paraphrase_xlm_r_multilingual_v1")
+    nlp_english = spacy.load("en_core_web_lg")
+    nlp_danish = spacy.load('da_core_news_lg')
+    nlp_english.add_pipe("sentence_bert", source=spacy.load("xx_paraphrase_xlm_r_multilingual_v1"))
+    nlp_danish.add_pipe("sentence_bert", source=spacy.load("xx_paraphrase_xlm_r_multilingual_v1"))
+    df['English Spacy'] = df['English'].apply(lambda x: nlp_english(x))
+    df['Danish Spacy'] = df['Danish'].apply(lambda x: nlp_danish(x))
+    df['Danish Spacy1'] = df['Danish Spacy'].shift(1)
+    df['Danish Spacy2'] = df['Danish Spacy'].shift(2)
+    df['Danish Spacy3'] = df['Danish Spacy'].shift(3)
+    df['Danish Spacy4'] = df['Danish Spacy'].shift(4)
+    df['Danish Spacy5'] = df['Danish Spacy'].shift(-1)
+    df['Danish Spacy6'] = df['Danish Spacy'].shift(-2)
+    df['Danish Spacy7'] = df['Danish Spacy'].shift(-3)
+    df['Danish Spacy8'] = df['Danish Spacy'].shift(-4)
+    df.fillna("", inplace=True)
+    #Try use Bleualign with splitting the documents at \n\n...
 
-    # Danish Parliament texts (separated)
-    """english_sentences, danish_sentences = get_language_sentences("english_eu.html",
-                                                                 "danish_eu.html")"""
+    # Dask
+    ddf = dd.from_pandas(df, npartitions=16)
+    for i in range(9):
+        if i == 0:
+            ddf['Sim_Scores'] = ddf.apply(calc_sim_score, axis=1, danish_column_name="Danish Spacy", meta=float)
+        elif i != 3 or i != 4 or i != 7 or i != 8:
+            ddf['Sim_Scores{}'.format(i)] = ddf.apply(calc_sim_score, axis=1, danish_column_name="Danish Spacy{}".format(i), meta=float)
+    df_csv_save = pd.DataFrame(ddf)
+    df_csv_save.to_csv("sim_scores_4up_4down.csv")
+    print(ddf.head())
+    print(ddf.columns)
+    for j, row in ddf.iterrows():
+        print(row['Sim_Scores'])
+        print(row['Sim_Scores1'])
+        print(row['Sim_Scores2'])
+        print(row['Sim_Scores3'])
+        print(row['Sim_Scores4'])
+        print(row['Sim_Scores5'])
+        print(row['Sim_Scores6'])
+        print(row['Sim_Scores7'])
+        print(row['Sim_Scores8'])
+        print("-----")
+        if j == 5:
+            break
+    end = time.time()
+    print("Number of partitions: {}".format(16))
+    print((end-start)/60)
 
-    # en_da (pandas parallel - not confirmed)
-    # df = pd.read_csv("en_da.csv")
+"""if __name__ == "__main__":
 
+    start = time.time()
     # da_en statistics (pandas parallel - confirmed)
     df = pd.read_csv("vikingeskibsmuseet_da_en_statistic.csv")
     gold_standard = {}
@@ -196,48 +287,57 @@ if __name__ == "__main__":
         gold_standard.update({row['English']: row['Danish']})
     df = pd.DataFrame(df[:150])
     gold_standard_df = pd.DataFrame(df)
+    end = time.time()
+    print("Time taken for loading: {} minutes.".format((end-start)/60))
 
-    nlp = spacy.load("xx_sent_ud_sm")
+    start = time.time()
+    nlp_multi = spacy.load("xx_paraphrase_xlm_r_multilingual_v1")
     nlp_danish = spacy.load('da_core_news_lg')
     # Base sentence segmentation code
-    """english_sentences = [nlp(sent) for sent in df['English'].to_list()[:100] if not isinstance(sent, float)]
-    danish_sentences = [nlp(sent) for sent in df['Danish'].to_list()[:100] if not isinstance(sent, float)]
-    """
+    #english_sentences = [nlp(sent) for sent in df['English'].to_list()[:100] if not isinstance(sent, float)]
+    #danish_sentences = [nlp(sent) for sent in df['Danish'].to_list()[:100] if not isinstance(sent, float)]
 
-    eng_split = nlp(' '.join(df['English'].to_list()))
+
+    eng_split = nlp_multi(' '.join(df['English'].to_list()))
     da_split = nlp_danish(' '.join(df['Danish'].to_list()))
     english_sentences = []
     danish_sentences = []
     for sent in eng_split.sents:
         english_sentences.append(sent)
     for sent in da_split.sents:
-        danish_sentences.append(nlp(sent.text))
-    """print("VERB TESTS ######################################")
-    for i, english_sentence in enumerate(english_sentences):
-        print(sim_score_verbs(english_sentence, danish_sentences[i]))
-    print("VERB TESTS ######################################")"""
+        danish_sentences.append(nlp_multi(sent.text))
+    end = time.time()
+    print("Time taken for intial Spacy Load: {} minutes.".format((end - start) / 60))
 
+    start = time.time()
     print("eng_split: {} \n english_sentences: {}".format(len(eng_split), len(english_sentences)))
     print("da_split: {} \n danish_sentences: {}".format(len(da_split), len(danish_sentences)))
     model = SentenceTransformer('sentence-transformers/stsb-xlm-r-multilingual')
-    gold_standard_df['Similarity Score'] = gold_standard_df.apply(lambda x: text_similarity.text_similarity(x['English'],
-                                                                                                            x['Danish'],
-                                                                                                            embedding_model=model),
-                                                                  axis=1)
+    #gold_standard_df['Similarity Score'] = gold_standard_df.apply(lambda x: text_similarity.text_similarity(x['English'],
+    #                                                                                                        x['Danish'],
+    #                                                                                                        embedding_model=model),
+    #                                                              axis=1)
+    ddf = dd.from_pandas(gold_standard_df, npartitions=16)
+
+    ddf['Similarity Score'] = ddf.apply(calc_sim_score, axis=1, meta=float)
+    gold_standard_df = pd.DataFrame(ddf)
     gold_standard_df.to_csv("vikingeskibsmuseet_gold_standard.csv", index=None)
 
     en_sentence_results, da_sentence_results = match_english_sentences(english_sentences,
                                                                        danish_sentences,
                                                                        model,
                                                                        search_space=3)
+    end = time.time()
+    print("Time taken for gold_standard: {} minutes.".format((end - start) / 60))
 
+    start = time.time()
     # English Sentences
     print("===ENGLISH RESULTS ===")
     for e in en_sentence_results:
         print(e)
         print("----")
-    repeat = 10
-    search_space = 3
+    repeat = 3
+    search_space = 2
     repeating_results = repeat_matching(en_sentence_results,
                                         english_sentences,
                                         danish_sentences,
@@ -247,6 +347,10 @@ if __name__ == "__main__":
                                         # Same with search space..
                                         search_space=search_space)
     en_repeating_results = sorted(repeating_results, key=lambda x: x[0][1])
+    end = time.time()
+    print("Time taken for repeating section: {} minutes.".format((end - start) / 60))
+
+    start = time.time()
     df_final_results_list = []
     for e in en_repeating_results:
         print("ENGLISH: {} \n DANISH: {}".format(e[0], e[1]))
@@ -255,14 +359,12 @@ if __name__ == "__main__":
     df_final_results = pd.DataFrame(df_final_results_list, columns=["English", "Danish"])
     df_final_results['Match'] = df_final_results.apply(lambda x: validate_results(x,
                                                                                   gold_standard=gold_standard), axis=1)
-    
+
     df_final_results['Sim_Score'] = df_final_results.apply(lambda x: text_similarity.text_similarity(x['English'],
                                                                                                      x['Danish'],
                                                                                                      embedding_model=model), axis=1)
     df_final_results.to_csv("vikingeskibsmuseet_final_matched_r{}_ss{}.csv".format(repeat, search_space), index=None)
     print(df_final_results)
     print(Counter(df_final_results['Match'].to_list()))
-    # Danish Sentences
-    """for d in da_sentence_results:
-        print(d)
-        print("====")"""
+    end = time.time()
+    print("Time taken for final matching: {} minutes.".format((end - start) / 60))"""
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import read_epub
+from translate import Translator
+from scipy.stats import wasserstein_distance
+translator = Translator(to_lang="en", from_lang="da")
 
 
+def create_translations():
+    danish = read_epub.extract_contents("Den_lille_prins.epub")
+    english = read_epub.extract_contents("The_little_prince.epub")
+    d_split = danish.split("\n\n\n")
+    e_split = english.split("\n\n")
+    tran_list = [translator.translate(d) for d in d_split]
+    with open("d_split.txt", "w", encoding="utf-8") as f:
+        f.writelines(d_split)
+    with open("e_split.txt", "w", encoding="utf-8") as f:
+        f.writelines(e_split)
+    with open("d_translated.txt", "w", encoding="utf-8") as f:
+        f.writelines(tran_list)
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    with open("d_split.txt") as f:
+        d_text = f.readlines()
+    with open("e_split.txt") as f:
+        e_text = f.readlines()
+    for i in range(100):
+        d_lengths = [len(t.split()) for t in d_text if len(t.split()) > i]
+        e_lengths = [len(t.split()) for t in e_text if len(t.split()) > i]
+        print(wasserstein_distance(d_lengths, e_lengths))
